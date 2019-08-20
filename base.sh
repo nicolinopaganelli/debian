85f1fe8ef40413dbad72832c34f3a6f4e348ef43
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2019-08-20 09:06:32 -0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -2,6 +2,13 @@
 
 su
 
+#set variables
+echo "what is your username?"
+read NAME
+
+#add user to sudo group
+usermod -aG sudo $NAME
+
 #set hostname
 apt update
 apt install ranger net-tools python3-pip
