9d49fed2a8d5554b4434ad2057af07cc2a387860
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2019-08-20 18:56:01 -0700

    Update base.sh

--- base.sh
+++ base.sh
@@ -11,4 +11,21 @@ usermod -aG sudo $NAME
 
 #set hostname
 apt update
-apt install ranger net-tools python3-pip
+apt install ranger net-tools python3-pip open-ssh ufw
+
+#set up fw
+echo -e "would you like to allow ssh through your firewall?\n(y/n)"
+read fw_answer
+if [ "$fw_answer" == "y" ]
+then
+  sudo ufw allow OpenSSH
+  sudo ufw enable
+elif [ "$fw_answer" == "n" ]
+then
+  echo "not allowing ssh"
+  sudo ufw enable
+else
+  echo "'y' not selected, not allowing ssh"
+  sudo ufw enable
+fi
+    
