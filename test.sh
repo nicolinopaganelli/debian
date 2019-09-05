9faa48f340b0115f9c62a9cf3434698ac9d1b805
nicolinopaganelli <54078029+nicolinopaganelli@users.noreply.github.com>
2019-09-05 10:34:56 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-echo -e "echo 'shutdown (h) or restart (r) ?'\nread answer\nsudo shutdown -$answer now" > /usr/bin/off
+echo -e "echo 'shutdown (h) or restart (r) ?'\nread answer\nsudo shutdown -\$answer now" > /usr/bin/off
 sudo chmod 711 /usr/bin/off
